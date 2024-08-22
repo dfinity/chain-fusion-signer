e54b20546325c4ea126c973bdf0b92d43eceeb23
Max Murphy <max@winning.black>
2024-08-22 17:36:36 +0200

    delete

--- src/shared/src/impls.rs
+++ src/shared/src/impls.rs
@@ -1,8 +1,5 @@
 use crate::types::custom_token::{CustomToken, CustomTokenId, Token};
 use crate::types::token::UserToken;
-use crate::types::user_profile::{
-    AddUserCredentialError, StoredUserProfile, UserCredential,
-};
 use crate::types::{
     ApiEnabled, Config, CredentialType, InitArg, MigrationProgress,
     Timestamp, TokenVersion, Version,
@@ -68,88 +65,6 @@ impl From<InitArg> for Config {
     }
 }
 
-impl TokenVersion for CustomToken {
-    fn get_version(&self) -> Option<Version> {
-        self.version
-    }
-
-    fn clone_with_incremented_version(&self) -> Self {
-        let mut cloned = self.clone();
-        cloned.version = Some(cloned.version.unwrap_or_default() + 1);
-        cloned
-    }
-
-    fn clone_with_initial_version(&self) -> Self {
-        let mut cloned = self.clone();
-        cloned.version = Some(1);
-        cloned
-    }
-}
-
-impl fmt::Display for CredentialType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            CredentialType::ProofOfUniqueness => write!(f, "ProofOfUniqueness"),
-        }
-    }
-}
-
-impl TokenVersion for StoredUserProfile {
-    fn get_version(&self) -> Option<Version> {
-        self.version
-    }
-
-    fn clone_with_incremented_version(&self) -> Self {
-        let mut cloned = self.clone();
-        cloned.version = Some(cloned.version.unwrap_or_default() + 1);
-        cloned
-    }
-
-    fn clone_with_initial_version(&self) -> Self {
-        let mut cloned = self.clone();
-        cloned.version = Some(1);
-        cloned
-    }
-}
-
-impl StoredUserProfile {
-    #[must_use]
-    pub fn from_timestamp(now: Timestamp) -> StoredUserProfile {
-        let credentials: BTreeMap<CredentialType, UserCredential> = BTreeMap::new();
-        StoredUserProfile {
-            credentials,
-            created_timestamp: now,
-            updated_timestamp: now,
-            version: None,
-        }
-    }
-
-    /// # Errors
-    ///
-    /// Will return Err if there is a version mismatch.
-    pub fn add_credential(
-        &self,
-        profile_version: Option<Version>,
-        now: Timestamp,
-        credential_type: &CredentialType,
-        issuer: String,
-    ) -> Result<StoredUserProfile, AddUserCredentialError> {
-        if profile_version != self.version {
-            return Err(AddUserCredentialError::VersionMismatch);
-        }
-        let mut new_profile = self.clone_with_incremented_version();
-        let user_credential = UserCredential {
-            credential_type: credential_type.clone(),
-            verified_date_timestamp: Some(now),
-            issuer,
-        };
-        let mut new_credentials = new_profile.credentials.clone();
-        new_credentials.insert(credential_type.clone(), user_credential);
-        new_profile.credentials = new_credentials;
-        new_profile.updated_timestamp = now;
-        Ok(new_profile)
-    }
-}
 
 
 impl Default for ApiEnabled {
