00e1f6a3411f160d928d57b9b674902049e70922
Max Murphy <max@winning.black>
2024-09-04 12:39:34 +0200

    Move tghe derivation path code into a dedicated file

---
+++ src/signer/src/derivation_path.rs
@@ -0,0 +1,18 @@
+use candid::Principal;
+
+/// The schema, which is the first part of a derivation path.
+#[repr(u8)]
+pub enum Schema {
+    /// The derivation path for a principal, with no further domain separation.
+    Principal = 1,
+}
+
+impl From<Schema> for Vec<u8> {
+    fn from(s: Schema) -> Vec<u8> {
+        vec![s as u8]
+    }
+}
+
+pub fn from_principal(p: &Principal) -> Vec<Vec<u8>> {
+    vec![Schema::Principal.into(), p.as_slice().to_vec()]
+}
