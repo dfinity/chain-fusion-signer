e95ede5d8d8541df45705d2d80e91c78770360d5
Max Murphy <max@winning.black>
2024-09-17 11:37:26 +0200

    clippy

--- src/signer/src/derivation_path.rs
+++ src/signer/src/derivation_path.rs
@@ -23,7 +23,7 @@ impl From<Schema> for Vec<u8> {
 
 impl Schema {
     /// The caller may specify any derivation path, as long as it starts with schema and caller principal.
-    pub fn derivation_path(&self, principal: &Principal) -> Vec<Vec<u8>> {
-        vec![(*self).into(), principal.as_slice().to_vec()]
+    pub fn derivation_path(self, principal: &Principal) -> Vec<Vec<u8>> {
+        vec![self.into(), principal.as_slice().to_vec()]
     }
 }
