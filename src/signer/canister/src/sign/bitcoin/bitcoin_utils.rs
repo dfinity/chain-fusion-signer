6fd1b7abff59ddc4293e05e5b5fd93db074dcec9
Llorenç Muntaner <llorenc.muntaner@gmail.com>
2024-09-23 15:47:23 +0200

    feat(signer): Change API to make it backwards compatible (#87)
    # Motivation
    
    Use P2WPKH instead of P2PKH and prepare the address and balance
    endpoints for bitcoin for frontwards compatibility by introducing
    request and response types.
    
    # Changes
    
    * New request and response types for getting the btc address and
    balance.
    * Change the bitcoin utils to return a Result instead of failing.
    * Change `caller_btc_address` and `caller_btc_balance` to use params and
    return Result.
    * Change `caller_btc_address` and `caller_btc_balance` to work only for
    P2WPKH address type.
    * Remove `public_key_to_p2pkh_address` and implement
    `principal_to_p2wpkh_address` instead.
    
    # Tests
    
    * Change the test to use new request and response types.
    * Change expected addresses because they are not the same with P2WPKH
    type.

--- src/signer/canister/src/sign/bitcoin/bitcoin_utils.rs
+++ src/signer/canister/src/sign/bitcoin/bitcoin_utils.rs
@@ -1,7 +1,7 @@
 //! Code for signing Bitcoin transactions.
 use crate::derivation_path::Schema;
 use crate::state::read_config;
-use bitcoin::{Address, Network, PublicKey};
+use bitcoin::{Address, CompressedPublicKey, Network};
 use candid::Principal;
 use ic_cdk::api::management_canister::bitcoin::BitcoinNetwork;
 use ic_cdk::api::management_canister::ecdsa::{
@@ -9,9 +9,9 @@ use ic_cdk::api::management_canister::ecdsa::{
 };
 
 /// Computes the public key of the specified principal.
-pub async fn ecdsa_pubkey_of(principal: &Principal) -> Vec<u8> {
+async fn ecdsa_pubkey_of(principal: &Principal) -> Result<Vec<u8>, String> {
     let name = read_config(|s| s.ecdsa_key_name.clone());
-    let (key,) = ecdsa_public_key(EcdsaPublicKeyArgument {
+    if let Ok((key,)) = ecdsa_public_key(EcdsaPublicKeyArgument {
         canister_id: None,
         derivation_path: Schema::Btc.derivation_path(principal),
         key_id: EcdsaKeyId {
@@ -20,8 +20,11 @@ pub async fn ecdsa_pubkey_of(principal: &Principal) -> Vec<u8> {
         },
     })
     .await
-    .expect("failed to get public key");
-    key.public_key
+    {
+        Ok(key.public_key)
+    } else {
+        Err("Failed to get ecdsa public key".to_string())
+    }
 }
 
 fn transform_network(network: BitcoinNetwork) -> Network {
@@ -33,11 +36,16 @@ fn transform_network(network: BitcoinNetwork) -> Network {
 }
 
 /// Converts a public key to a P2PKH address.
-/// Reference: [IC Bitcoin Documentation](https://internetcomputer.org/docs/current/developer-docs/multi-chain/bitcoin/using-btc/generate-addresses#generating-addresses-with-threshold-ecdsa)
-pub fn public_key_to_p2pkh_address(network: BitcoinNetwork, public_key: &[u8]) -> String {
-    Address::p2pkh(
-        PublicKey::from_slice(public_key).expect("failed to parse public key"),
-        transform_network(network),
-    )
-    .to_string()
+pub async fn principal_to_p2wpkh_address(
+    network: BitcoinNetwork,
+    principal: &Principal,
+) -> Result<String, String> {
+    let ecdsa_pubkey = ecdsa_pubkey_of(principal)
+        .await
+        .map_err(|_| "Error getting ECDSA public key".to_string())?;
+    if let Ok(compressed_public_key) = CompressedPublicKey::from_slice(&ecdsa_pubkey) {
+        Ok(Address::p2wpkh(&compressed_public_key, transform_network(network)).to_string())
+    } else {
+        Err("Error getting P2WPKH from public key".to_string())
+    }
 }
