1664491f2486147911578fab881dc49457ce522d
Max Murphy <max@winning.black>
2024-10-16 06:08:48 +0200

    fmt

--- scripts/bind/pic.sh
+++ scripts/bind/pic.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
-canisters=( $(jq -r '.canisters|keys|.[]' dfx.json) )
+canisters=($(jq -r '.canisters|keys|.[]' dfx.json))
 for canister in "${canisters[@]}"; do
   pic_binding_builder="./scripts/bind/pic/${canister}.sh"
-  if test -f "$pic_binding_builder"
-  then echo "INFO: Creating pic bindings for $canister..."
+  if test -f "$pic_binding_builder"; then
+    echo "INFO: Creating pic bindings for $canister..."
     bash -x "$pic_binding_builder"
   else
-    
     echo "INFO: No pic binding script for $canister at $pic_binding_builder"
   fi
 done
