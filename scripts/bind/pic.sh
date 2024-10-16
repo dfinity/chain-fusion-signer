6adcc786edf4f40524682c6f52698eec150616cf
Max Murphy <max@winning.black>
2024-10-16 06:08:18 +0200

    ++

---
+++ scripts/bind/pic.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+canisters=( $(jq -r '.canisters|keys|.[]' dfx.json) )
+for canister in "${canisters[@]}"; do
+  pic_binding_builder="./scripts/bind/pic/${canister}.sh"
+  if test -f "$pic_binding_builder"
+  then echo "INFO: Creating pic bindings for $canister..."
+    bash -x "$pic_binding_builder"
+  else
+    
+    echo "INFO: No pic binding script for $canister at $pic_binding_builder"
+  fi
+done
