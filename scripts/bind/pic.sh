0a6adff6631bf431efab9b0f6dca52c084f360c9
Max Murphy <max@winning.black>
2024-10-16 10:16:49 +0200

    lint

--- scripts/bind/pic.sh
+++ scripts/bind/pic.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-canisters=($(jq -r '.canisters|keys|.[]' dfx.json))
+mapfile -t canisters < <(jq -r '.canisters|keys|.[]' dfx.json)
 for canister in "${canisters[@]}"; do
   pic_binding_builder="./scripts/bind/pic/${canister}.sh"
   if test -f "$pic_binding_builder"; then
--- scripts/bind/pic/cycles_depositor.sh
+++ scripts/bind/pic/cycles_depositor.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env bash
 didc bind -t rs "$(jq -r .canisters.cycles_depositor.candid dfx.json)" --config scripts/bind/pic/cycles_depositor.toml >src/signer/canister/tests/it/utils/cycles_depositor.rs
