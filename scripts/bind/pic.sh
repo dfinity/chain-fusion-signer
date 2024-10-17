91c24a291001a2cc60526eaa36c4e5ed3e7ffafc
Max Murphy <max@winning.black>
2024-10-17 09:31:56 +0200

    tweak

--- scripts/bind/pic.sh
+++ scripts/bind/pic.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+[[ "${1:-}" != "--help" ]] || {
+  cat <<-EOF
+	Generates candid file and javascript bindings for the signer
+	EOF
+
+  exit 0
+}
+
 mapfile -t canisters < <(jq -r '.canisters|keys|.[]' dfx.json)
 for canister in "${canisters[@]}"; do
   pic_binding_builder="./scripts/bind/pic/${canister}.sh"
