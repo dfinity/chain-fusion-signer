2b926402804325e08f7639a1e2a7be26653dc820
Max Murphy <max@winning.black>
2024-10-29 11:35:44 +0100

    ++

--- src/signer/canister/tests/it/eth.rs
+++ src/signer/canister/tests/it/eth.rs
@@ -1,7 +1,10 @@
 use crate::{
     canister::{
         cycles_ledger::{self, ApproveArgs},
-        signer::{EthSignPrehashResponse, EthSignTransactionRequest, PaymentType},
+        signer::{
+            EthPersonalSignRequest, EthPersonalSignResponse, EthSignPrehashResponse,
+            EthSignTransactionRequest, PaymentType,
+        },
     },
     utils::{
         mock::{CALLER, CALLER_ETH_ADDRESS, SEPOLIA_CHAIN_ID},
@@ -14,8 +17,10 @@ use candid::{Nat, Principal};
 use ic_chain_fusion_signer_api::{methods::SignerMethods, types::transaction::SignRequest};
 
 #[test]
-fn test_eth_sign_transaction() {
+fn can_eth_sign_transaction() {
     let test_env = TestSetup::default();
+
+    let caller = Principal::from_text(CALLER).unwrap();
 
     let sign_request = &EthSignTransactionRequest {
         chain_id: Nat::from(SEPOLIA_CHAIN_ID),
@@ -28,7 +33,6 @@ fn test_eth_sign_transaction() {
         data: None,
     };
 
-    let caller = Principal::from_text(CALLER).unwrap();
     let payment_type = PaymentType::CallerPaysIcrc2Cycles;
     let payment_recipient = cycles_ledger::Account {
         owner: test_env.signer.canister_id(),
@@ -40,45 +44,80 @@ fn test_eth_sign_transaction() {
         .icrc_2_approve(caller, &ApproveArgs::new(payment_recipient, amount.into()))
         .expect("Failed to call ledger canister")
         .expect("Failed to approve payment");
-    let transaction = test_env
+
+    let response = test_env
         .signer
         .eth_sign_transaction(caller, sign_request, &Some(payment_type))
-        .unwrap();
+        .expect("Failed to reach the signer canister")
+        .expect("Failed to sign");
 
     assert_eq!(
-        transaction.unwrap(),
+        response,
         EthSignPrehashResponse{ signature: "0x02f86783aa36a7808203158201c87b945e9f1caf942aa8ee887b75f5a6bccaf4b10242480180c001a0fc97df3cb643abb3b565cd95b8d55f108db336612abbb79e0054588587306809a04014551c96d89a90ff89065f08b05772cd582d26e3e1eee4ccf85d2fedc2ad50".to_string()}
     );
 }
 
-#[ignore] // TODO: Update this test
 #[test]
-fn test_personal_sign() {
-    let pic_setup = setup();
+fn can_eth_personal_sign() {
+    let test_env = TestSetup::default();
 
     let caller = Principal::from_text(CALLER).unwrap();
 
-    let transaction = pic_setup.update_one::<String>(
-        caller,
-        "personal_sign",
-        hex::encode("test message".to_string()),
-    );
+    let request = EthPersonalSignRequest {
+        message: hex::encode("test message"),
+    };
+
+    let payment_type = PaymentType::CallerPaysIcrc2Cycles;
+    let payment_recipient = cycles_ledger::Account {
+        owner: test_env.signer.canister_id(),
+        subaccount: None,
+    };
+    let amount: u64 = SignerMethods::EthPersonalSign.fee() + LEDGER_FEE as u64;
+    test_env
+        .ledger
+        .icrc_2_approve(caller, &ApproveArgs::new(payment_recipient, amount.into()))
+        .expect("Failed to call ledger canister")
+        .expect("Failed to approve payment");
+
+    let result = test_env
+        .signer
+        .eth_personal_sign(caller, &request, &Some(payment_type))
+        .expect("Failed to call the signer canister")
+        .expect("Failed to sign");
 
     assert_eq!(
-        transaction.unwrap(),
-        "0xdfa9da1c6e67a7d77b85c8afd0d83cf3b4d095d69055586626be2c83abfdd8423279306df72a7c069b0ad23120765186d0334238a25464d6f705c91e3238418401".to_string()
+        result,
+        EthPersonalSignResponse {
+            signature: "0x91f0caeca09d8520c905be5287e3fd13fcd355f17fdec41d72430b5bd6c5274266a2840f693377c853f36bc7b82f9e353d8da53e2c8530250f85adf5551268e800".to_string()
+        }
     );
 }
 
-#[ignore] // TODO: Update this test
 #[test]
-fn test_cannot_personal_sign_if_message_is_not_hex_string() {
-    let pic_setup = setup();
+fn cannot_personal_sign_if_message_is_not_hex_string() {
+    let test_env = TestSetup::default();
 
     let caller = Principal::from_text(CALLER).unwrap();
 
-    let result =
-        pic_setup.update_one::<String>(caller, "personal_sign", "test message".to_string());
+    let request = EthPersonalSignRequest {
+        message: "test message".to_string(),
+    };
+
+    let payment_type = PaymentType::CallerPaysIcrc2Cycles;
+    let payment_recipient = cycles_ledger::Account {
+        owner: test_env.signer.canister_id(),
+        subaccount: None,
+    };
+    let amount: u64 = SignerMethods::EthPersonalSign.fee() + LEDGER_FEE as u64;
+    test_env
+        .ledger
+        .icrc_2_approve(caller, &ApproveArgs::new(payment_recipient, amount.into()))
+        .expect("Failed to call ledger canister")
+        .expect("Failed to approve payment");
+
+    let result = test_env
+        .signer
+        .eth_personal_sign(caller, &request, &Some(payment_type));
 
     assert!(result.is_err());
     assert!(result.unwrap_err().contains("failed to decode hex"));
