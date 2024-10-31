6975a3d221722aa1d6f551998635596cf2a3ed00
Max Murphy <max@winning.black>
2024-10-31 18:18:43 +0100

    Move btc tests into amodule

--- src/signer/canister/tests/it/bitcoin.rs
+++ src/signer/canister/tests/it/bitcoin.rs
@@ -5,27 +5,30 @@ use ic_chain_fusion_signer_api::types::bitcoin::{
     BitcoinAddressType, GetBalanceError, GetBalanceRequest, GetBalanceResponse,
 };
 
-#[ignore] // TODO: Update this test
-#[test]
-fn test_caller_btc_balance() {
-    let pic_setup = setup();
+mod caller_balance {
+    use super::*;
+    #[ignore] // TODO: Update this test
+    #[test]
+    fn test_caller_btc_balance() {
+        let pic_setup = setup();
 
-    let caller = Principal::from_text(CALLER).unwrap();
-    let network = BitcoinNetwork::Regtest;
-    let params = GetBalanceRequest {
-        network,
-        address_type: BitcoinAddressType::P2WPKH,
-        min_confirmations: None,
-    };
+        let caller = Principal::from_text(CALLER).unwrap();
+        let network = BitcoinNetwork::Regtest;
+        let params = GetBalanceRequest {
+            network,
+            address_type: BitcoinAddressType::P2WPKH,
+            min_confirmations: None,
+        };
 
-    let balance_response = pic_setup
-        .update_one::<Result<GetBalanceResponse, GetBalanceError>>(
-            caller,
-            "btc_caller_balance",
-            params,
-        )
-        .expect("Failed to call testnet btc balance.")
-        .expect("Failed to get successul balance response");
+        let balance_response = pic_setup
+            .update_one::<Result<GetBalanceResponse, GetBalanceError>>(
+                caller,
+                "btc_caller_balance",
+                params,
+            )
+            .expect("Failed to call testnet btc balance.")
+            .expect("Failed to get successul balance response");
 
-    assert_eq!(balance_response.balance, 0u64);
+        assert_eq!(balance_response.balance, 0u64);
+    }
 }
