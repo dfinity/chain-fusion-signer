193dfe7f08807baece030539fb838efea0af9911
Max Murphy <max@winning.black>
2024-11-04 11:36:28 +0100

    ++

--- src/signer/canister/tests/it/utils/mock.rs
+++ src/signer/canister/tests/it/utils/mock.rs
@@ -3,9 +3,4 @@ pub const CALLER_ETH_ADDRESS: &str = "0xDFB554B25A5fC2F44aEc0fCd8b541F065Ac33C0a
 pub const CALLER_BTC_ADDRESS_MAINNET: &str = "bc1qmrfmudl3j6jy3jf9aqvsk47cf3nyqw5kwx0nhx"; // Depends on pocket-ic version.
 pub const CALLER_BTC_ADDRESS_TESTNET: &str = "tb1qmrfmudl3j6jy3jf9aqvsk47cf3nyqw5kyq5qv4";
 pub const CALLER_BTC_ADDRESS_REGTEST: &str = "bcrt1qmrfmudl3j6jy3jf9aqvsk47cf3nyqw5kxfddmu";
-
-/// An admin user.  Typically, controls the backend canister.
-pub const CONTROLLER: &str = "l3lfs-gak7g-xrbil-j4v4h-aztjn-4jyki-wprso-m27h3-ibcl3-2cwuz-oqe";
-/// A normal user, without any special permissions.
-
 pub const SEPOLIA_CHAIN_ID: u64 = 11155111;
--- src/signer/canister/tests/it/utils/pocketic.rs
+++ src/signer/canister/tests/it/utils/pocketic.rs
@@ -1,17 +1,4 @@
-use crate::utils::pic_canister::PicCanisterTrait;
-use candid::{encode_one, CandidType, Principal};
-use ic_cdk::api::management_canister::bitcoin::BitcoinNetwork;
-use ic_chain_fusion_signer_api::types::{Arg, InitArg};
-use pocket_ic::{CallError, PocketIc, PocketIcBuilder};
-use std::{
-    env,
-    fs::read,
-    path::{Path, PathBuf},
-    sync::Arc,
-    time::Duration,
-};
 
-use super::mock::CONTROLLER;
 
 // Default file paths relative to the cargo workspace.
 pub const BITCOIN_CANISTER_ID: &str = "g4xu7-jiaaa-aaaan-aaaaq-cai";
