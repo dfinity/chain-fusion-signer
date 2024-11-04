b4897cd6db5cc29d7e2d35058d60eb4e49de7c3a
Max Murphy <max@winning.black>
2024-11-04 11:35:46 +0100

    ++

--- src/signer/canister/tests/it/utils/pocketic.rs
+++ src/signer/canister/tests/it/utils/pocketic.rs
@@ -15,22 +15,3 @@ use super::mock::CONTROLLER;
 
 // Default file paths relative to the cargo workspace.
 pub const BITCOIN_CANISTER_ID: &str = "g4xu7-jiaaa-aaaan-aaaaq-cai";
-
-// This is necessary to deploy the bitcoin canister.
-// This is a struct based on the `InitConfig` from the Bitcoin canister.
-// Reference: https://github.com/dfinity/bitcoin-canister/blob/52c160168c478d5bce34b7dc5bacb78243c9d8aa/interface/src/lib.rs#L553
-//
-// The only field that matters is `network`. The others are considered dummy and set to `None` anyway.
-#[derive(CandidType)]
-struct BitcoinInitConfig {
-    stability_threshold: Option<u64>,
-    network: Option<BitcoinNetwork>,
-    blocks_source: Option<String>,
-    syncing: Option<String>,
-    fees: Option<String>,
-    api_access: Option<String>,
-    disable_api_if_not_fully_synced: Option<String>,
-    watchdog_canister: Option<Principal>,
-    burn_cycles: Option<String>,
-    lazily_evaluate_fee_percentiles: Option<String>,
-}
