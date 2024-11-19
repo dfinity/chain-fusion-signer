d9e7e712e5c99a08e6fff410ef9d43af65787ccc
Max Murphy <max@winning.black>
2024-11-19 22:30:47 +0100

    ++

--- src/signer/cli/src/main.rs
+++ src/signer/cli/src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+use signer_cli::args::SignerCliArgs;
+use signer_cli::execute;
+use tokio::runtime::Builder;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args = SignerCliArgs::parse();
+    let runtime = Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .expect("Unable to create a runtime");
+    runtime.block_on(async {
+        if let Err(err) = execute(&args).await {
+            println!("Failed to execute command: {}", err);
+            std::process::exit(1);
+        }
+    });
 }
