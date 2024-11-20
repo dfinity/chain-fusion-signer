514bfc3d456a0890e009be3e880975c0e0757559
Max Murphy <max@winning.black>
2024-11-20 10:51:12 +0100

    ++

--- src/signer/cli/src/lib.rs
+++ src/signer/cli/src/lib.rs
@@ -11,11 +11,11 @@ use slog::Logger;
 pub mod args;
 pub mod logger;
 use anyhow::Context;
-use ic_cdk::api::management_canister::{ecdsa::{
-    EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument, EcdsaPublicKeyResponse,
-}, schnorr::{SchnorrAlgorithm, SchnorrKeyId, SchnorrPublicKeyResponse}};
+use ic_cdk::api::management_canister::{
+    ecdsa::{EcdsaCurve, EcdsaKeyId, EcdsaPublicKeyArgument, EcdsaPublicKeyResponse},
+    schnorr::{SchnorrAlgorithm, SchnorrKeyId, SchnorrPublicKeyArgument, SchnorrPublicKeyResponse, SignWithSchnorrArgument, SignWithSchnorrResponse},
+};
 use ic_chain_fusion_signer_api::types::generic::GenericCallerEcdsaPublicKeyError;
-use ic_cdk::api::management_canister::schnorr::SchnorrPublicKeyArgument;
 
 pub struct SignerCli {
     dfx_interface: DfxInterface,
@@ -121,7 +121,6 @@ impl SignerCli {
         Ok(response)
     }
 
-
     pub async fn schnorr_public_key(&self) -> anyhow::Result<SchnorrPublicKeyResponse> {
         let signer_canister_id = self
             .canister_id("signer")
@@ -157,5 +156,37 @@ impl SignerCli {
         Ok(response)
     }
 
-
+    pub async fn schnorr_sign(&self, message: &[u8]) -> anyhow::Result<Vec<u8>> {
+        let signer_canister_id = self
+            .canister_id("signer")
+            .expect("Signer canister ID is not known");
+        let key_name = "dfx_test_key".to_string(); // TODO: "key_1" on mainnet
+        let response_bytes = self
+            .dfx_interface
+            .agent()
+            .update(&signer_canister_id, "schnorr_sign")
+            .with_arg(
+                candid::encode_one(SignWithSchnorrArgument {
+                    message: message.to_vec(),
+                    derivation_path: vec![],
+                    key_id: SchnorrKeyId {
+                        algorithm: SchnorrAlgorithm::Ed25519,
+                        name: "test_key_1".to_string(),
+                    },
+                })
+                .with_context(|| "Failed to encode argument")?,
+            )
+            .call_and_wait()
+            .await
+            .with_context(|| "Failed to make canister call")?;
+        let response = candid::decode_one::<
+            Result<(SignWithSchnorrResponse,), GenericCallerEcdsaPublicKeyError>,
+        >(&response_bytes)
+        .with_context(|| "Failed to decode response")?;
+        let response = match response {
+            Ok((response,)) => response,
+            Err(err) => panic!("Failed to sign: {:?}", err),
+        };
+        Ok(response.signature)
+    }
 }
