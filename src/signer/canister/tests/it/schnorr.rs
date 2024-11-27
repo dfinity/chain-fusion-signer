adf9f40b491080a90c501021460457181c71ca3a
Max Murphy <max@winning.black>
2024-11-27 18:47:53 +0100

    ++

--- src/signer/canister/tests/it/schnorr.rs
+++ src/signer/canister/tests/it/schnorr.rs
@@ -99,14 +99,18 @@ fn signatures_can_be_verified() {
         SchnorrKeyId {
             algorithm: SchnorrAlgorithm::Ed25519,
             name: "dfx_test_key".to_string(),
-        }
+        },
+        SchnorrKeyId {
+            algorithm: SchnorrAlgorithm::Bip340Secp256K1,
+            name: "dfx_test_key".to_string(),
+        },
     ];
     let cost_per_user = Nat::from({
-       let num_tests = derivation_paths.len() * key_types.len();
-       let cost_per_test = SignerMethods::SchnorrPublicKey.fee()
+        let num_tests = derivation_paths.len() * key_types.len();
+        let cost_per_test = SignerMethods::SchnorrPublicKey.fee()
             + SignerMethods::SchnorrSign.fee()
             + 2 * LEDGER_FEE as u64;
-         num_tests as u64 * cost_per_test
+        num_tests as u64 * cost_per_test
     });
 
     let message = ByteBuf::from("pokemon");
@@ -125,72 +129,96 @@ fn signatures_can_be_verified() {
             )
             .expect("Failed to call ledger canister")
             .expect("Failed to approve payment");
-        for derivation_path in derivation_paths.iter() {
-            let public_key = test_env
-                .signer
-                .schnorr_public_key(
-                    *user,
-                    &SchnorrPublicKeyArgument {
-                        key_id: SchnorrKeyId {
-                            algorithm: SchnorrAlgorithm::Ed25519,
-                            name: "dfx_test_key".to_string(),
+        for key_type in key_types.iter() {
+            for derivation_path in derivation_paths.iter() {
+                let public_key = test_env
+                    .signer
+                    .schnorr_public_key(
+                        *user,
+                        &SchnorrPublicKeyArgument {
+                            key_id: SchnorrKeyId {
+                                algorithm: SchnorrAlgorithm::Ed25519,
+                                name: "dfx_test_key".to_string(),
+                            },
+                            canister_id: None,
+                            derivation_path: derivation_path.clone(),
                         },
-                        canister_id: None,
-                        derivation_path: derivation_path.clone(),
-                    },
-                    &Some(PaymentType::PatronPaysIcrc2Cycles(signer::Account {
-                        owner: test_env.user,
-                        subaccount: None,
-                    })),
-                )
-                .unwrap()
-                .unwrap()
-                .0
-                .public_key
-                .into_vec();
+                        &Some(PaymentType::PatronPaysIcrc2Cycles(signer::Account {
+                            owner: test_env.user,
+                            subaccount: None,
+                        })),
+                    )
+                    .unwrap()
+                    .unwrap()
+                    .0
+                    .public_key
+                    .into_vec();
 
-            let signature = test_env
-                .signer
-                .schnorr_sign(
-                    *user,
-                    &signer::SignWithSchnorrArgument {
-                        key_id: SchnorrKeyId {
-                            algorithm: SchnorrAlgorithm::Ed25519,
-                            name: "dfx_test_key".to_string(),
+                let signature = test_env
+                    .signer
+                    .schnorr_sign(
+                        *user,
+                        &signer::SignWithSchnorrArgument {
+                            key_id: key_type.clone(),
+                            derivation_path: derivation_path.clone(),
+                            message: message.clone(),
                         },
-                        derivation_path: derivation_path.clone(),
-                        message: message.clone(),
-                    },
-                    &Some(PaymentType::PatronPaysIcrc2Cycles(signer::Account {
-                        owner: test_env.user,
-                        subaccount: None,
-                    })),
-                )
-                .unwrap()
-                .unwrap()
-                .0
-                .signature
-                .into_vec();
+                        &Some(PaymentType::PatronPaysIcrc2Cycles(signer::Account {
+                            owner: test_env.user,
+                            subaccount: None,
+                        })),
+                    )
+                    .unwrap()
+                    .unwrap()
+                    .0
+                    .signature
+                    .into_vec();
 
-            // Verify the signature
-            {
-                verify_schnorr_ed25519_signature(&signature, &public_key, &message);
+                // Verify the signature
+                match &key_type.algorithm {
+                    SchnorrAlgorithm::Bip340Secp256K1 => {
+                        verify_schnorr_bip340_secp256k1_signature(
+                            &signature,
+                            &public_key,
+                            &message,
+                        );
+                    }
+                    SchnorrAlgorithm::Ed25519 => {
+                        verify_schnorr_ed25519_signature(&signature, &public_key, &message);
+                    }
+                }
             }
         }
     }
 }
 
-
-fn verify_schnorr_ed25519_signature(signature_bytes: &[u8], public_key_bytes: &[u8], message_bytes: &[u8]) {
+fn verify_schnorr_ed25519_signature(
+    signature_bytes: &[u8],
+    public_key_bytes: &[u8],
+    message_bytes: &[u8],
+) {
     let signature = ed25519_dalek::Signature::try_from(signature_bytes)
         .expect("failed to deserialize signature");
     let verifying_key = ed25519_dalek::VerifyingKey::from_bytes(
-        &<[u8; 32]>::try_from(public_key_bytes)
-            .expect("ed25519 public key should be 32 bytes"),
+        &<[u8; 32]>::try_from(public_key_bytes).expect("ed25519 public key should be 32 bytes"),
     )
     .expect("failed to sec1 deserialize public key");
 
     use ed25519_dalek::Verifier;
     let verification = verifying_key.verify(message_bytes, &signature);
-    assert!(verification.is_ok(), "Not verified: {:?}", verification);
+    assert!(verification.is_ok(), "Ed25519 signature not verified: {:?}", verification);
+}
+
+fn verify_schnorr_bip340_secp256k1_signature(
+    signature_bytes: &[u8],
+    public_key_bytes: &[u8],
+    message_bytes: &[u8],
+) {
+    let signature = k256::schnorr::Signature::try_from(signature_bytes)
+        .expect("failed to deserialize signature");
+    let verifying_key = k256::schnorr::VerifyingKey::from_bytes(&public_key_bytes)
+        .expect("failed to deserialize public key");
+
+    let verification = verifying_key.verify_raw(&message_bytes, &signature);
+    assert!(verification.is_ok(), "Schnorr bip340 secp256k1 signature not verified: {:?}", verification);
 }
