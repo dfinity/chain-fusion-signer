f8d2c1ecf1cda24c0669267c956379790eb0fa70
Max Murphy <max@winning.black>
2024-11-27 12:16:42 +0100

    ++

--- src/signer/canister/tests/it/schnorr.rs
+++ src/signer/canister/tests/it/schnorr.rs
@@ -1,7 +1,9 @@
 //! Tests the Schnorr signing API.
 
 use std::collections::HashMap;
-use crate::canister::signer::SchnorrPublicKeyArgument;
+use serde_bytes::ByteBuf;
+
+use crate::canister::signer::{SchnorrKeyId, SchnorrPublicKeyArgument, SchnorrAlgorithm, PaymentType};
 use crate::utils::test_environment::TestSetup;
 
 
@@ -10,34 +12,36 @@ use crate::utils::test_environment::TestSetup;
 fn public_keys_are_different() {
     let test_env = TestSetup::default();
     let users = &test_env.users;
-    let derivation_paths = vec![
+    let derivation_paths: Vec<Vec<ByteBuf>> = [
         vec![],
-        vec![vec![]],
-        vec![vec![], vec![]],
-        vec![vec![], vec![], vec![]],
-        vec!["foo".to_owned().into_bytes()],
-        vec!["foo".to_owned().into_bytes(), vec![]],
-        vec![vec![], "foo".to_owned().into_bytes()],
-        vec![vec![], "foo".to_owned().into_bytes(), vec![]],
-        vec!["f".to_owned().into_bytes(), "oo".to_owned().into_bytes()],
-    ];
-/*
-    let public_keys = HashMap::new();
+        vec![""],
+        vec!["", ""],
+        vec!["", "", ""],
+        vec!["foo"],
+        vec!["foo", ""],
+        vec!["", "foo"],
+        vec!["", "foo", ""],
+        vec!["f", "oo"],
+    ].into_iter().map(|paths| paths.into_iter().map(ByteBuf::from).collect()).collect();
+    let mut public_keys = HashMap::new();
     for user in users.iter() {
         for derivation_path in derivation_paths.iter() {
             let public_key = test_env
                 .signer
-                .schnorr_public_key(SchnorrPublicKeyArgument {
+                .schnorr_public_key(*user, &SchnorrPublicKeyArgument {
+                    key_id: SchnorrKeyId{
+                        algorithm: SchnorrAlgorithm::Ed25519,
+                        name: "test_key_1".to_string(),
+                    },
                     canister_id: None,
                     derivation_path: derivation_path.clone(),
-                })
-                .await
+                }, &Some(PaymentType::AttachedCycles))
+                .unwrap()
                 .unwrap()
                 .0
                 .public_key;
-            assert!(!public_keys.contains_key(&public_key));
+            assert!(!public_keys.contains_key(&public_key), "These have the same public key: {:?} and {:?}", public_keys.get(&public_key), (user, derivation_path));
             public_keys.insert(public_key, (*user, derivation_path.clone()));
         }
     }
-    */
 }
