571c86431b011611f497ed101f8191e8a47c745f
Max <bitdivine@users.noreply.github.com>
2024-11-28 15:59:32 +0100

    feat(signer): Schnorr Signatures (#196)
    # Motivation
    The Internet Computer has a canister API for creating Schnorr
    signatures. This should be exposed by the chain fusion signer, just like
    every other threshold signing API.
    
    # Changes
    - Add API methods for getting Schnorr public keys and signatures.
    
    # Tests
    - Tests are included:
    - to verify that different users and derivation paths have different
    keys.
    - to verify that signatures can be verified with the appropriate public
    key.
    
    We propose to add examples that demonstrate how to use this API with
    other chains, such as Solana, in another PR.

---
+++ src/signer/api/src/types/schnorr.rs
@@ -0,0 +1,55 @@
+//! Types for the Schnorr signing API.
+use candid::{CandidType, Deserialize};
+use ic_cdk::api::call::RejectionCode;
+
+#[derive(CandidType, Deserialize, Debug, Clone)]
+pub enum SchnorrSigningError {
+    /// Payment failed.
+    PaymentError(ic_papi_api::PaymentError),
+    /// An `ic_cdk::call::CallResult` error received when making the canister thereshold signature
+    /// API call.
+    SigningError(RejectionCode, String),
+}
+impl From<ic_papi_api::PaymentError> for SchnorrSigningError {
+    fn from(e: ic_papi_api::PaymentError) -> Self {
+        Self::PaymentError(e)
+    }
+}
+impl From<(RejectionCode, String)> for SchnorrSigningError {
+    fn from((rejection_code, message): (RejectionCode, String)) -> Self {
+        Self::SigningError(rejection_code, message)
+    }
+}
+
+#[derive(CandidType, Deserialize, Debug, Clone)]
+pub enum SchnorrPublicKeyError {
+    /// Payment failed.
+    PaymentError(ic_papi_api::PaymentError),
+    /// An `ic_cdk::call::CallResult` error received when making the canister thereshold signature
+    /// API call.
+    SigningError(RejectionCode, String),
+}
+impl From<ic_papi_api::PaymentError> for SchnorrPublicKeyError {
+    fn from(e: ic_papi_api::PaymentError) -> Self {
+        Self::PaymentError(e)
+    }
+}
+impl From<(RejectionCode, String)> for SchnorrPublicKeyError {
+    fn from((rejection_code, message): (RejectionCode, String)) -> Self {
+        Self::SigningError(rejection_code, message)
+    }
+}
+
+#[derive(CandidType, Deserialize, Debug, Clone)]
+pub enum SchnorrSignWithEcdsaError {
+    /// Payment failed.
+    PaymentError(ic_papi_api::PaymentError),
+    /// An `ic_cdk::call::CallResult` error received when making the canister thereshold signature
+    /// API call.
+    SigningError(RejectionCode, String),
+}
+impl From<ic_papi_api::PaymentError> for SchnorrSignWithEcdsaError {
+    fn from(e: ic_papi_api::PaymentError) -> Self {
+        Self::PaymentError(e)
+    }
+}
