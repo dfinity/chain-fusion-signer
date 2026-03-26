18fec06ffd45ea2995a215dc241cbd6c8d7993b7
Antonio Ventilii <antonio.ventilii@dfinity.org>
2026-03-26 13:04:08 +0100

    auth

--- src/example_frontend/src/App.js
+++ src/example_frontend/src/App.js
@@ -1,5 +1,5 @@
-import { Actor, HttpAgent } from '@dfinity/agent';
-import { AuthClient } from '@dfinity/auth-client';
+import { Actor, HttpAgent } from '@icp-sdk/core/agent';
+import { AuthClient } from '@icp-sdk/auth/client';
 import {
 	example_backend,
 	idlFactory as exampleBackendIdlFactory
--- src/example_frontend/vite.config.js
+++ src/example_frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 		emptyOutDir: true
 	},
 	optimizeDeps: {
-		esbuildOptions: {
+		rolldownOptions: {
 			define: {
 				global: 'globalThis'
 			}
